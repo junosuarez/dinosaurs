32390dab067dc483398b3227b076ab67bc490e68
jden <jason@denizac.org>
2015-02-13 01:06:17 -0800

    garrr relative paths

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 var fs = require('fs')
+var path = require('path')
 
-module.exports = fs.readFileSync('./dinosaurs.csv', 'utf-8').split('\n').filter(Boolean)
+module.exports = fs.readFileSync(path.join(__dirname, 'dinosaurs.csv'), 'utf-8').split('\n').filter(Boolean)
